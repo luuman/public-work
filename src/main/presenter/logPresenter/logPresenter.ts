0742660c0009adc82ec0ab52d68945d8e1a7aca0
zhongcheng.li <luumans@qq.com>
2025-08-17 21:48:42 +0800

    feat: done

--- src/main/presenter/logPresenter/logPresenter.ts
+++ src/main/presenter/logPresenter/logPresenter.ts
@@ -11,7 +11,6 @@ export class LogPresenter {
 
   constructor() {
     const originalConsole = { ...console }
-
     ;['log', 'warn', 'error', 'debug', 'info'].forEach((method) => {
       console[method] = (...args) => {
         const timestamp = new Date().toISOString()
