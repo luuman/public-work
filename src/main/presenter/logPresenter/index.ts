651ddaa7ce8d5cd1dcbd291e48ed4e2e0a21c637
zhongcheng.li <luumans@qq.com>
2025-08-17 21:31:52 +0800

    done

--- src/main/presenter/logPresenter/index.ts
+++ src/main/presenter/logPresenter/index.ts
@@ -3,3 +3,12 @@ import { logPresenter } from './logPresenter'
 export const screenshot = logPresenter.getLogger('screenshot')
 export const appLog = logPresenter.getLogger()
 export const msgAllLog = logPresenter.getLogger('msgAllLog')
+
+const originalConsole = { ...console }
+
+;['log', 'warn', 'error', 'debug', 'info'].forEach((method) => {
+  console[method] = (...args) => {
+    const timestamp = new Date().toISOString()
+    originalConsole[method](`[${timestamp}]`, ...args)
+  }
+})
